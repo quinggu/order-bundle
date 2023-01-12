58fbbe7647c9e3ab19e997bba971f1852f1235a1
Tomek Białczak <tomasz.bialczak@burdamedia.pl>
2023-01-12 18:39:50 +0100

    tests

---
+++ tests/OrderStatusServiceTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class OrderStatusServiceTest extends TestCase
+{
+    //TODO: some tests
+
+//    public function testCheckStatus(): void
+//    {
+//        $orderStatusService = new OrderStatusService();
+//        $response = $orderStatusService->checkStatus();
+//        $this->assertSame(200, $response->getStatusCode());
+//    }
+}
